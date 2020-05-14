522c7d39a5d359edfe721eafb0988a427337f0b2
jangwoni79 <s2018w17@e-mirim.hs.kr>
2020-05-14 09:53:45 +0900

    views BookmarkList

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from bookmark.views import BookmarkList
+
 app_name = 'bookmark'
 urlpatterns = [
     path('', BookmarkList.as_view(), name='list'),  # bookmark:list #{% url 'bookmark:list' %}
